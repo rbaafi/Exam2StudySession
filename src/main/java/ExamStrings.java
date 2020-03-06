6a127c87386f9a26774fe420a4e1a70431c5f8a4
Richmond <rbaafi@cnm.edu>
2020-03-06 07:11:49 -0700

    initial commit.

--- src/main/java/ExamStrings.java
+++ src/main/java/ExamStrings.java
@@ -1,21 +1,33 @@
+import static jdk.nashorn.internal.objects.NativeString.indexOf;
+import static jdk.nashorn.internal.objects.NativeString.slice;
+
 import java.util.List;
 
-public class ExamStrings {
+class Exam2Strings {
 
-  String string;
+  public static void main(String[] args) {
+    int[] testArray = new int[100];
 
-  ExamStrings(String s) {
-    string = s;
+    for (int i = 0; i < 100; i++) {
+      testArray[i] = i;
+    }
+    printArray(slice(testArray, 50, 10, 50));
   }
 
-  public String trimWideSpace() {
-    List<Character>
+  static void printArray(int[] array) {
+    for (int i: array) {
+      System.out.println((i + ""));
+    }
   }
+  static int[] slice(int[] array, int to, int from, int stride) {
+    int newLength = (int)Math.ceil((double) (to - from) / (double) stride);
+    int[] newInt = new int[newLength];
 
-  String reverse(String toReverse) {
-   char[] backward = toReverse.toCharArray();
-   
-    return "";
+    int iterator = 0;
+    for (int i = from; i < to; i += stride) {
+      newInt[iterator++] = array[i];
+    }
+    return newInt;
   }
 }
 
